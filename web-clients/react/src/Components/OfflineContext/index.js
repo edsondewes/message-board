5c98ba7ebfd9fe76eb39959f8d7c440e5c8d7f4c
Edson Dewes <edson_dewes@hotmail.com>
2018-05-19 16:55:47 -0300

    [react] Created OfflineContext to detect online/offline browser events

---
+++ web-clients/react/src/Components/OfflineContext/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const DEFAULT_ONLINE_VALUE = true;
+
+const OfflineContext = React.createContext({
+  online: DEFAULT_ONLINE_VALUE,
+});
+
+const OfflineConsumer = OfflineContext.Consumer;
+
+class OfflineProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      online:
+        typeof window !== "undefined" ? navigator.onLine : DEFAULT_ONLINE_VALUE,
+    };
+
+    this.updateOnlineStatus = this.updateOnlineStatus.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("online", this.updateOnlineStatus);
+    window.addEventListener("offline", this.updateOnlineStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("online", this.updateOnlineStatus);
+    window.removeEventListener("offline", this.updateOnlineStatus);
+  }
+
+  updateOnlineStatus() {
+    this.setState({
+      online: navigator.onLine,
+    });
+  }
+
+  render() {
+    return (
+      <OfflineContext.Provider value={this.state}>
+        {this.props.children}
+      </OfflineContext.Provider>
+    );
+  }
+}
+
+class OfflinePanel extends React.Component {
+  render() {
+    return (
+      <OfflineConsumer>
+        {context => !context.online && this.props.children}
+      </OfflineConsumer>
+    );
+  }
+}
+
+export { OfflineConsumer, OfflinePanel, OfflineProvider };
