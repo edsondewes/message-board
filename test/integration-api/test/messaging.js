4b966ef32a2a1b4e1a27fe1b3343ff506338bd1a
Edson Dewes <edson_dewes@hotmail.com>
2018-06-26 21:26:12 -0300

    [tests] Use mocha context instead of a helper module

--- test/integration-api/test/messaging.js
+++ test/integration-api/test/messaging.js
@@ -1,6 +1,5 @@
 const expect = require("chai").expect;
 const axios = require("axios");
-const context = require("../helpers/testContext");
 
 //TODO: use some config file or env to customize the URL
 const apiUrl = "http://localhost:5000/api/messages";
@@ -20,8 +19,6 @@ describe("Messaging API", function() {
 
       expect(response.status).to.equal(200);
       expect(response.data).to.have.all.keys("id", "created", "text");
-
-      context.addMessage(response.data);
     });
 
     it("should reject text longer than 250 characteres", async function() {
@@ -44,12 +41,19 @@ describe("Messaging API", function() {
   });
 
   describe("GET message by id", function() {
+    before(async function() {
+      const response = await axios.post(apiUrl, {
+        text: "test message by id",
+      });
+
+      this.message = response.data;
+    });
+
     it("should return the same data as the POST method", async function() {
-      const contextMessage = context.listMessages()[0];
-      const response = await axios.get(`${apiUrl}/${contextMessage.id}`);
+      const response = await axios.get(`${apiUrl}/${this.message.id}`);
 
       expect(response.status).to.equal(200);
-      expect(response.data).to.deep.equal(contextMessage);
+      expect(response.data).to.deep.equal(this.message);
     });
 
     it("should return not found for an unknown id", async function() {
@@ -65,35 +69,32 @@ describe("Messaging API", function() {
   describe("GET paginated messages", function() {
     before(async function() {
       //create some messages before testing the pagination
+      this.messages = [];
       for (let index = 0; index < 5; index++) {
         var response = await axios.post(apiUrl, {
           text: `test message ${index}`,
         });
 
-        context.addMessage(response.data);
+        this.messages.unshift(response.data);
       }
     });
 
     it("should return the last created messages if 'from' is not specified", async function() {
       const response = await axios.get(apiUrl);
-
-      const contextMessages = context.listMessages();
-      const responseMessages = response.data.slice(0, contextMessages.length);
-
-      expect(contextMessages).to.deep.equal(responseMessages);
+      const responseMessages = response.data.slice(0, this.messages.length);
+      expect(this.messages).to.deep.equal(responseMessages);
     });
 
     it("should return the messages created before the 'from'", async function() {
-      const contextMessages = context.listMessages();
       const fromIndex = 1;
 
       const response = await axios.get(apiUrl, {
         params: {
-          from: contextMessages[fromIndex].id,
+          from: this.messages[fromIndex].id,
         },
       });
 
-      const paginatedContext = contextMessages.slice(fromIndex + 1);
+      const paginatedContext = this.messages.slice(fromIndex + 1);
       const responseMessages = response.data.slice(0, paginatedContext.length);
 
       expect(paginatedContext).to.deep.equal(responseMessages);
